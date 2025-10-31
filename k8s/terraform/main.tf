6887f86c8c64c5e2ce10822f4c770878ddd5f272
Daniel Rojo <danielrojo@gmail.com>
2025-10-31 10:12:07 +0100

    docs update

---
+++ k8s/terraform/main.tf
@@ -0,0 +1,88 @@
+# Crear un namespace (opcional)
+resource "kubernetes_namespace" "nginx" {
+  metadata {
+    name = "nginx-namespace"
+  }
+}
+
+# Deployment de nginx
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name      = "nginx-deployment"
+    namespace = kubernetes_namespace.nginx.metadata[0].name
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:1.21.6"
+          name  = "nginx"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/"
+              port = 80
+            }
+            initial_delay_seconds = 3
+            period_seconds        = 3
+          }
+        }
+      }
+    }
+  }
+}
+
+# Service para exponer nginx
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name      = "nginx-service"
+    namespace = kubernetes_namespace.nginx.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "nginx"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+      node_port   = 30201
+    }
+
+    type = "NodePort"
+  }
+}
